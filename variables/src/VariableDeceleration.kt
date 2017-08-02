72499cf5f517b1aad4e8a9ff7b11eccd23ebc585
govindavaram srikanth <govindavaramsrikanth@gmail.com>
2017-08-02 22:13:15 +0530

    Add files via upload

---
+++ variables/src/VariableDeceleration.kt
@@ -0,0 +1,29 @@
+/**
+ * Created by govinds on 7/19/2017.
+ */
+
+fun main(agr: Array<String>){
+
+
+  Var  myString =”Hello world
+  Var my no = 10
+  Var dec = 1.0
+
+  var a: Byte=10
+    a.toInt()
+    var b: Int=10
+    b.toByte()
+
+    val b: Byte = 1 // OK, literals are checked statically
+    val i: Int = b // ERROR
+
+
+  var a: String = "abc"
+  a = null // compilation error
+
+  var b: String? = "abc"
+  b = null // ok
+
+
+
+}
